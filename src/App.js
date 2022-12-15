8cb00e94dc89bd22e4b963997234763e0b6b61d3
Yuya Hochi <yuya1charo@gmail.com>
2022-12-15 18:03:20 +0000

    made some changes to App.js and install mui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import React, {useState}from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [num, setNum] = useState(0);
+
+  const incrementNumber = () => {
+    setNum(prev => prev + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{num}</h2>
+      <button onClick = {incrementNumber}>Increment</button>
     </div>
   );
 }
