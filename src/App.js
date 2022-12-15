68b33e8e54e163bb1b91bf9d89c2ee604d492fbc
Yuya Hochi <yuya1charo@gmail.com>
2022-12-15 19:08:31 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Homepage from "./pages/Homepage"
+import Homepage from "./pages/homepage"
 import Register from './pages/register'
 
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./Header.css";
+import "./header.css";
 
 // Logo, Home Button, Profile, Notifications, Chat.
 
