10054191d5408cbe7c603bdd2c897778e09f432b
leonkwan46 <66780058+leonkwan46@users.noreply.github.com>
2022-12-16 17:14:01 +0000

    Register Page Done

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,40 +1,83 @@
 import React from 'react';
-import { Button, TextField } from '@mui/material';
-import { Form, Formik, Field } from 'formik';
-import { Box } from '@mui/system';
+import { Button, Grid, TextField } from '@mui/material';
+import { Form, Formik } from 'formik';
+import { Box, Container } from '@mui/system';
+import { registerValidation } from '../../src/validations/registerValidation'
 
 const Register = () => {
 
-    return (
-        <Formik
-            initialValues={{username: "", password: "", confirm_password: ""}}
-            onSubmit = { async (values) => {
-                alert(JSON.stringify(values))
-            }}
-        >
-            {({values, handleChange}) => (
-            <Form>
-                <Box>
-                    <Field 
-                    onChange={handleChange} 
-                    value={values.username}
-                    id = 'username'
-                    label = 'Username'
-                    />
-                </Box>
-                <Box>
-                    <Field 
-                    onChange={handleChange} 
-                    value={values.password}
-                    id = 'password'
-                    label = 'Password'
-                    />
-                </Box>
+    const checkError = (touched, errors) => {
+        if (touched && errors) {
+          return true;
+        }
+        return false;
+      };
 
-                <Button>Submit</Button>
-            </Form>
-            )}
-        </Formik>
+    return (
+        <Container>
+            <Grid container justifyContent={"center"} textAlign={'center'}>
+                <Grid paddingTop={30}>
+                    <Formik
+                        initialValues={{ username: "", password: "", confirm_password: "" }}
+                        onSubmit={async (values) => {
+                            //TODO: Build API to BackEnd
+                            alert(JSON.stringify(values))
+                        }}
+                        validationSchema={registerValidation}
+                    >
+                        {({ values, handleChange, handleBlur, touched, errors, handleSubmit }) => (
+                            <Form>
+                                    <Box padding={1}>
+                                        <TextField
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.username}
+                                            id='username'
+                                            label='Username' 
+                                            error={checkError(touched.username, errors.username)}
+                                            helperText={
+                                                checkError(touched.username, errors.username) ? errors.username
+                                                :""
+                                            }
+                                            />
+                                    </Box>
+                                    <Box padding={1}>
+                                        <TextField
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.password}
+                                            id='password'
+                                            label='Password'
+                                            type='password'
+                                            error={checkError(touched.password, errors.password)}
+                                            helperText={
+                                                checkError(touched.password, errors.password) ? errors.password
+                                                :""
+                                            }                                    />
+                                    </Box>
+                                    <Box padding={1}>
+                                        <TextField
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.confirm_password}
+                                            id='confirm_password'
+                                            label='Confirm Password'
+                                            type='password'
+                                            error={checkError(touched.confirm_password, errors.confirm_password)}
+                                            helperText={
+                                                checkError(touched.confirm_password, errors.confirm_password) ? errors.confirm_password
+                                                :""
+                                            }                                    />
+                                    </Box>
+                                    <Box padding={1}>
+                                        <Button variant='contained' size='large' onClick={handleSubmit} >Register</Button>
+                                    </Box>
+                            </Form>
+                        )}
+                    </Formik>
+                </Grid>
+            </Grid>
+        </Container>
     )
 };
 
