9cfa094d3ce698158617ba32aafeb6d2e0002a3e
mmkk28 <kanok.kij28@gmail.com>
2022-12-17 15:47:56 +0000

    'm'

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,7 +4,8 @@ import { useFormik } from 'formik';
 import { Box, Container } from '@mui/system';
 import { loginValidation } from '../../src/validations/loginValidation'
 
-export default function MyApp() {
+export default function Login() {
+    
   return (
     <div>
       <Button variant="contained">Hello World</Button>
