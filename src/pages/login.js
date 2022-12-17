cfe50d6d6f83a2568975892851ac7454ecc2cd55
mmkk28 <kanok.kij28@gmail.com>
2022-12-17 17:47:20 +0000

    'm'

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import { Button} from '@mui/material';
-//import { useFormik } from 'formik';
-//import { Box, Container } from '@mui/system';
-//import { loginValidation } from '../../src/validations/loginValidation'
+import {Button, TextField, Typography} from '@mui/material';
+import { useFormik } from 'formik';
+import { Box, Container } from '@mui/system';
+import { loginValidation } from '../../src/validations/loginValidation'
 
-export default function Login() {
+const LoginForm = () =>{
+  const formik = useFormik({
+    initialValues:{
+      username: '',
+      password: '',
+    },
+    validationSchema: loginValidation,
+    onSubmit: values => {
+    alert(console.log);
+    },
+  });
+return (
+  <Container maxWidth="sm">
+    <Box sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+      }}>
+      <Typography component="h1" variant='h2'> Sign In </Typography>
+      <Typography component="h2" variant='h4'> Please login before using!</Typography>  
+    </Box>
+    <Box sx={{
+      marginTop: 4,
+      alignItems: 'center',
+    }}
+    onSubmit={formik.handleSubmit}>
+      <TextField required id = "username" label="Username" />;
+      <TextField required id = "password" label="Password" type="password" />
+      <Button variant='contained' type='submit'> submit </Button>
+    </Box>
 
-  return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+    </Container>
   );
 }
 
 
+export {LoginForm};
