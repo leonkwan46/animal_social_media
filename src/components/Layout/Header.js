aa5566276904bf4f47719737a2815c2f6e083bc5
Yuya Hochi <yuya1charo@gmail.com>
2022-12-19 13:02:42 +0000

    change filename of Header.js and Header.css to header.js and header.css

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,8 +3,10 @@ import React from 'react'
 import Logo from "../../assets/images/animal-society-low-resolution-logo-color-on-transparent-background.webp";
 
 import "./header.css";
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
+import IconButton from '@mui/material/IconButton';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import PersonIcon from '@mui/icons-material/Person';
 
 // Logo, Home Button, Profile, Notifications, Chat.
 
@@ -14,11 +16,11 @@ const Header = () => {
       <div className="logo-container">
         <img src = {Logo} alt = "logo" className='logo'/>
       </div>
-      <ButtonGroup variant="contained" aria-label="outlined primary button group">
-        <Button>Chat</Button>
-        <Button>Notification</Button>
-        <Button>Profile</Button>
-      </ButtonGroup>
+      <div className="button-group">
+        <IconButton><ChatBubbleOutlineIcon /></IconButton>
+        <IconButton><NotificationsNoneIcon /></IconButton>
+        <IconButton><PersonIcon /></IconButton>
+      </div>
     </header>
   )
 }
