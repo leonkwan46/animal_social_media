ec875b6c26859b3bfeb91b17f50fccf992b287d7
mmkk28 <kanok.kij28@gmail.com>
2023-01-17 15:21:46 +0000

    'rebuilt the deleted bakcend'

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,52 +1,48 @@
 const express = require('express')
-const body = require('body-parser')
-const cors = require('cors')
-require('./db/config')
-require('dotenv').config()
 
-const User = require('./db/users')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+// const User = require('./db/users')
+// const jwt = require('jsonwebtoken')
+// const bcrypt = require('bcrypt')
 
-const { authenticateToken } = require('./middleware/authMiddleware')
+// const { authenticateToken } = require('./middleware/authMiddleware')
 
-
-const bodyParser = require('body-parser')
 const app = express()
 
-app.use(body.urlencoded({extended:true}))
-app.use(bodyParser.json())
-app.use(express.json());
-app.use(cors());
+// app.use(body.urlencoded({extended:true}))
+// app.use(bodyParser.json())
+// app.use(express.json());
+// app.use(cors());
 
-app.get('/test', (req, res) => {
-    const data = {
-        haha: 'haha'
-    }
-    res.json({data})
-})
+// app.get('/test', (req, res) => {
+//     const data = {
+//         haha: 'haha'
+//     }
+//     res.json({data})
+// })
 
-app.post('/register', async(req, res) => {
-    // const hash = bcrypt.hash(req.bodypassword, salt) 
-    const data = new User({
-        username: req.body.username,
-        password: req.body.password,
-    })
-    try {
-        // data.save();
-        jwt.sign({data}, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.json({token});
-            }
-        })
-    } catch (err) {
-        res.json("Register FAILED")
-    }
-})
+// app.post('/register', async(req, res) => {
+//     // const hash = bcrypt.hash(req.bodypassword, salt) 
+//     const data = new User({
+//         username: req.body.username,
+//         password: req.body.password,
+//     })
+//     try {
+//         // data.save();
+//         jwt.sign({data}, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 res.json({token});
+//             }
+//         })
+//     } catch (err) {
+//         res.json("Register FAILED")
+//     }
+// })
 
 
-app.listen(5000, () => {
-    console.log("http://localhost:5000");
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
