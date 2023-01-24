18b3b03fab1bd3937481829a769a2637ce503928
Leon Kwan <leonkwan46@gmail.com>
2023-01-24 15:40:16 +0000

    Update after Meeting

---
+++ BackEnd/middleware/errorHandler.js
@@ -0,0 +1,8 @@
+const errorHandler= (err, req, res, next) => {
+    console.log('====================================');
+    console.log(err.message);
+    console.log('====================================');
+    res.status(err.status || 500).json(err.message || "Internal Server Error")
+}
+
+module.exports = errorHandler
