bd9cb617d80d72d1bb9f8340acc9d97459a2f00a
Yuya Hochi <yuya1charo@gmail.com>
2023-02-01 00:27:44 +0000

    modify createPost and homepage.css, homepage in backend

--- FrontEnd/src/components/createPost.js
+++ FrontEnd/src/components/createPost.js
@@ -1,73 +1,157 @@
-import React, {useState} from 'react'
-import { Formik, Form } from 'formik';
+import React, { useState } from "react";
+import { Formik, Form } from "formik";
+
+import axios from "axios";
 
 import profilePic from "../assets/images/charo.jpg";
-import { TextField, Button, Avatar } from '@mui/material';
-import SendIcon from '@mui/icons-material/Send';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import { TextField, Button, Avatar } from "@mui/material";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import SendIcon from "@mui/icons-material/Send";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 import "./createPost.css";
 
 const user = {
-    picture: profilePic,
-    alt: "black-hand",
-    
-}
+  picture: profilePic,
+  alt: "black-hand",
+};
 
 const maxLength = 280;
 
+const backURL = "http://localhost:5000/homepage";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={8} ref={ref} variant="filled" {...props} />;
+});
+
 const Post = () => {
-  const handleSubmit = (values) => {
-    alert(values.text);
-  }
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const onSubmit = async (values, { setValues }) => {
+    try {
+      if (values.length === 0)
+        throw new Error("You need to write more than 1 letter!");
+      await axios
+        .post(
+          backURL,
+          { text: values.text },
+          {
+            headers: {
+              authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.status);
+          handleOpen();
+          setValues({ text: "", length: 0, percent: 0 });
+        })
+        .catch((err) => console.log(err));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Formik
-      onSubmit={handleSubmit}
+      onSubmit={onSubmit}
       initialValues={{ text: "", length: 0, percent: 0 }}
     >
-      { ({values, handleChange, handleSubmit}) => (
-      <Form className = "post-wrapper">
-      <div className="post-upper">
-        <Avatar sx={{width: "50px", height: "50px"}} src={user.picture} alt={user.alt} className ="profile-pic-for-post"/>
+      {({ values, handleChange, handleSubmit }) => (
+        <Form className="post-wrapper">
+          <div className="post-upper">
+            <Avatar
+              sx={{ width: "50px", height: "50px" }}
+              src={user.picture}
+              alt={user.alt}
+              className="profile-pic-for-post"
+            />
             <TextField
               value={values.text}
               onChange={(e) => {
                 values.text += e.target.value;
                 values.length = e.target.value.length;
-                values.percent = Math.floor(e.target.value.length / maxLength * 100)
+                values.percent = Math.floor(
+                  (e.target.value.length / maxLength) * 100
+                );
                 handleChange(e);
               }}
-              id = "text"
+              id="text"
               label="What's new with you?"
               className="post"
               multiline="true"
               inputProps={{ maxLength: maxLength }}
               fullWidth
-         />
-      </div>
-      <div className="post-lower">
-        <div className="circular-container">
-          {
-            values.length !== 0 ? <CircularProgressbar
-              value={values.percent}
-              text={`${maxLength - values.length <= 30 ? maxLength - values.length : ""}`} 
-              strokeWidth="12"
-              styles={buildStyles({
-                textSize: "2em", textWeight: "700",
-                pathColor: `${maxLength - values.length < 10 ? "#ff3d00"
-                           : (maxLength - values.length < 30 ? "#ffff00" : "#3e98c7")}`
-              })}
-              /> : ""
-          }
-        </div>
-        <Button type = "submit" variant="contained" size = "medium" endIcon={<SendIcon />}>Send</Button>
-      </div>
-      </Form>
+            />
+          </div>
+          <div className="post-lower">
+            <div className="circular-container">
+              {values.length !== 0 ? (
+                <CircularProgressbar
+                  value={values.percent}
+                  text={`${
+                    maxLength - values.length <= 30
+                      ? maxLength - values.length
+                      : ""
+                  }`}
+                  strokeWidth="12"
+                  styles={buildStyles({
+                    textSize: "2em",
+                    textWeight: "700",
+                    pathColor: `${
+                      maxLength - values.length < 10
+                        ? "#ff3d00"
+                        : maxLength - values.length < 30
+                        ? "#ffff00"
+                        : "#3e98c7"
+                    }`,
+                  })}
+                />
+              ) : (
+                ""
+              )}
+            </div>
+            <Button
+              type="submit"
+              disabled={values.length === 0 ? true : false}
+              variant="contained"
+              size="medium"
+              endIcon={<SendIcon />}
+            >
+              Send
+            </Button>
+            <Snackbar
+              open={open}
+              autoHideDuration={2500}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+            >
+              <Alert onClose={handleClose} severity="info">
+                This is a success message!
+              </Alert>
+            </Snackbar>
+          </div>
+        </Form>
       )}
     </Formik>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
--- FrontEnd/src/pages/homepage.js
+++ FrontEnd/src/pages/homepage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Post from "../components/createPost.js";
 import TopNav from "../components/TopNav";
-
+import Feed from "../components/Feed";
 import "./homepage.css";
 import { Box, Container } from "@mui/system";
 
@@ -9,8 +9,12 @@ const Homepage = () => {
   return (
     <Container disableGutters={true} maxWidth={false} className="homepage">
       <TopNav />
-      <Box className="left-side">
-        <Post />
+      <Box className="body-wrapper">
+        <Box className="left-side">
+          <Post />
+          <Feed />
+        </Box>
+        <Box className="right-side">fdaf</Box>
       </Box>
     </Container>
   );
