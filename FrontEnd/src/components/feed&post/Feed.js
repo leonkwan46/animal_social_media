e1404540f35b129ae14d84cc63c474d49eadd006
Yuya Hochi <yuya1charo@gmail.com>
2023-02-02 14:24:32 +0000

    use useFetch in Feed.js

--- FrontEnd/src/components/feed&post/Feed.js
+++ FrontEnd/src/components/feed&post/Feed.js
@@ -1,51 +1,25 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Post from "./post";
-import { Grid, Typography } from "@mui/material";
+import useFetch from "../../hooks/usefetch";
+import { Typography } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
-import { Box, Container } from "@mui/system";
+import { Box } from "@mui/system";
+import Post from "./post";
 import "./feed.css";
 
 const Feed = () => {
-  const [status, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
   const backURL = "http://localhost:5000/homepage";
-  const accessToken = localStorage.getItem("token");
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        setLoading(true);
-        axios
-          .get(backURL, {
-            headers: {
-              authorization: "Bearer " + accessToken,
-            },
-          })
-          .then((res) => {
-            const message = JSON.stringify(res.data);
-            console.log(res.data);
-            console.log(message);
-            setData(res.data);
-            setLoading(false);
-          });
-        console.log(status);
-        console.log(status.length);
-      } catch (err) {
-        alert("Error retrieving data!!!");
-      }
-    };
-    getData();
-  }, []);
+  const { data, loading, error } = useFetch(backURL, {
+    headers: {
+      authorization: "Bearer " + localStorage.getItem("token"),
+    },
+  });
 
   return (
-    // ({handleSubmit}) => {
-
     <div className="post-wrapper">
       <Box>
         {loading ? <CircularProgress /> : ""}
-        {status.length > 0 ? (
-          status.map((p) => <Post key={p._id} post={p} />)
+        {!loading && data && data.length > 0 ? (
+          data.map((p) => <Post key={p._id} post={p} />)
         ) : loading ? (
           ""
         ) : (
@@ -54,7 +28,7 @@ const Feed = () => {
       </Box>
       <br />
       <Box>
-        {!loading && status.length > 0 ? (
+        {!loading && data && data.length > 0 ? (
           <Typography> - That's all. - </Typography>
         ) : (
           <Typography></Typography>
--- FrontEnd/src/hooks/usefetch.js
+++ FrontEnd/src/hooks/usefetch.js
@@ -15,6 +15,7 @@ const useFetch = (url, headers) => {
         .get(url, headers)
         .then((res) => {
           setData(res.data);
+          console.log(res.data);
         })
         .catch((err) => {
           setError(err);
