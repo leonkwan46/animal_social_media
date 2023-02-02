a0e424f093cb0bf669dbcf45d8a47a3233766b60
Yuya Hochi <yuya1charo@gmail.com>
2023-02-02 14:51:01 +0000

    accept incoming chagne

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,45 +1,48 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const connectDB = require('./db/config')
-const errorHandler = require('./middleware/errorHandler')
-<<<<<<< HEAD
-
-require('dotenv').config()
-=======
-const multer = require('multer')
-require('dotenv').config();
->>>>>>> e1404540f35b129ae14d84cc63c474d49eadd006
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connectDB = require("./db/config");
+const errorHandler = require("./middleware/errorHandler");
+const multer = require("multer");
 
 //Middlewares (App-level)
-<<<<<<< HEAD
-=======
-app.use(express.urlencoded({ extended: true }));
->>>>>>> e1404540f35b129ae14d84cc63c474d49eadd006
-app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "50mb",
+    parameterLimit: 1000000000,
+  })
+);
+app.use(express.json({ limit: "50mb" }));
 app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb', parameterLimit: 1000000000 }));
-
+// app.use(bodyParser.json({ limit: "50mb" }));
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//     limit: "50mb",
+//     parameterLimit: 1000000000,
+//   })
+// );
 
 // MongoDB Connection
 connectDB();
 
 // Register Router
-const registerRoute = require('./routes/register')
-app.use('/register', registerRoute)
+const registerRoute = require("./routes/register");
+app.use("/register", registerRoute);
 
 //Login Router
-const loginRoute = require('./routes/login')
-app.use('/login', loginRoute)
+const loginRoute = require("./routes/login");
+app.use("/login", loginRoute);
 
 // Profile Router
-const profileRoute = require('./routes/profile')
-app.use('/profile', profileRoute)
+const profileRoute = require("./routes/profile");
+app.use("/profile", profileRoute);
 
 //create, get post Router
-const homepageRoute = require('./routes/homepage')
-app.use('/homepage', homepageRoute)
+const homepageRoute = require("./routes/homepage");
+app.use("/homepage", homepageRoute);
 
 // Handle following a user and being Followed by a user
 const network = require("./routes/network");
@@ -47,10 +50,10 @@ app.use("/network", network);
 
 // Error Handling middleware always at LAST
 // Can only use on Routes/Endpoints, not DB Connection
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(5000, () => {
-    console.log("http://localhost:5000");
+  console.log("http://localhost:5000");
 });
 
 //JSON web token (note)
