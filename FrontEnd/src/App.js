7c96f976ca83df315dd2e127818a0b5606ab7a09
Leon Kwan <leonkwan46@gmail.com>
2023-02-21 15:36:45 +0000

    Implementing Welcome page

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,33 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./homepage/pages/homepage";
-import Login from "./login-register/pages/login";
+import WelcomePage from "./welcomepage/pages/welcomepage";
 import Profile from "./profile/pages/profile";
-import ResetPassword from "./login-register/components/ResetPassword";
-import {SocketContext, socket} from './shared/contexts/context';
-import {UserContext} from './shared/contexts/username'
+import ResetPassword from "./welcomepage/components/ResetPassword";
+import { SocketContext, socket } from "./shared/contexts/context";
+import { UserContext } from "./shared/contexts/username";
 import useFetch from "./shared/hooks/usefetch";
- import "./App.css";
-
+import "./App.css";
 
 const App = () => {
   const { data } = useFetch("http://localhost:5000/homepage/username", {
     headers: {
-      authorization: "Bearer " + localStorage.getItem("token")
-    }
+      authorization: "Bearer " + localStorage.getItem("token"),
+    },
   });
 
   return (
     <SocketContext.Provider value={socket}>
       <UserContext.Provider value={{ username: data?.username }}>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/profile/:id" element={<Profile />}  />
-      </Routes>
-    </Router>
-    </UserContext.Provider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/welcome" element={<WelcomePage />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/profile/:id" element={<Profile />} />
+          </Routes>
+        </Router>
+      </UserContext.Provider>
     </SocketContext.Provider>
   );
 };
